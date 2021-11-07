77b2dc59f5ed624a057af348a1c227dfa0382249
Nykorrin <aznmantran@gmail.com>
2021-11-07 01:32:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Venti
+[![Discord](https://img.shields.io/discord/213517939343622146.svg?colorB=7289da&label=discord&logo=discord&logoColor=white)](https://discord.gg/6aBFhAHUs7)
+
+A bard that seems to have arrived on some unknown wind — sometimes sings songs as old as the hills, and other times sings poems fresh and new.
---
+++ src/main/java/me/nykorrin/venti/Venti.java
@@ -0,0 +1,47 @@
+package me.nykorrin.venti;
+
+import me.nykorrin.venti.listeners.MemberListener;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+import java.util.Collections;
+
+public class Venti {
+
+    public static void main(String[] args) {
+        try {
+            JDA jda = JDABuilder.createDefault("OTA2ODA3MTk1MDQyMjA1NzA2.YYeAAg.F4soNiCMIqESRLTmdmtTfXCsM4o", Collections.emptyList())
+                    .addEventListeners(new MemberListener())
+                    .enableIntents(GatewayIntent.GUILD_VOICE_STATES)
+                    .enableCache(CacheFlag.VOICE_STATE)
+                    .setActivity(Activity.playing("some music..."))
+                    .build();
+
+            CommandListUpdateAction commands = jda.updateCommands();
+
+            commands.addCommands(
+                    new CommandData("play", "Play some tunes!")
+                            .addOptions(new OptionData(OptionType.STRING, "song", "Song URL/Name")
+                                    .setRequired(true))
+            );
+            commands.addCommands(
+                    new CommandData("stop", "Stop the tunes.")
+            );
+            commands.addCommands(
+                    new CommandData("skip", "Skip a song.")
+            );
+            commands.queue();
+            jda.awaitReady();
+        } catch (LoginException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
